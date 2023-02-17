3fdf2191b3dc733c2427d10eef50e22e3ef959ff
Tirth24052002 <tirth.p@simformsolutions.com>
2023-02-17 09:15:36 +0530

    toolbar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.myTollBar);
+        setSupportActionBar(toolbar);
     }
 }
