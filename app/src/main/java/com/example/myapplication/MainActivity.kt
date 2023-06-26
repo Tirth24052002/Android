aa7005e4447253790362a193937d3396f6978dfc
Tirth24052002 <tirth.p@simformsolutions.com>
2023-06-26 18:58:49 +0530

    View and View Group, buttons, toast and layouts

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,32 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import android.content.Intent
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.myapplication.uiwidgets.ViewsActivity
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
+    private var toolbar: Toolbar? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.myTollBar)
+        setSupportActionBar(toolbar)
+           setupNavigationButtons()
+    }
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        toolbar=findViewById(R.id.myTollBar);
-        setSupportActionBar(toolbar);
+    private fun setupNavigationButtons() {
+            val uiWidgetsButton: Button = findViewById(R.id.viewButton);
+            uiWidgetsButton.setOnClickListener {
+                val intent = Intent(this, ViewsActivity::class.java)
+                startActivity(intent)
+            }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/uiwidgets/ConstraintLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.uiwidgets
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
