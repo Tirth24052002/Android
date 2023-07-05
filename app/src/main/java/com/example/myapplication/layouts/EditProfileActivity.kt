e922d6ba7697c9e48ee3235d03dfc4eeb83dfaa5
Tirth24052002 <tirth.p@simformsolutions.com>
2023-07-05 13:07:32 +0530

    Constraint layout Edit Profile Screen Completed custom error in edit text field

--- app/src/main/java/com/example/myapplication/layouts/EditProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/layouts/EditProfileActivity.kt
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import com.example.myapplication.R
 
@@ -23,12 +24,19 @@ class EditProfileActivity : AppCompatActivity() {
     lateinit var perState: EditText
     lateinit var perZipCode: EditText
     lateinit var perMobileNo: EditText
+    lateinit var fnError: TextView
+    lateinit var lnError: TextView
+    lateinit var adrError: TextView
+    lateinit var cityError: TextView
+    lateinit var stateError: TextView
+    lateinit var zipError: TextView
+    lateinit var mobileError: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         spinnerSetup()
-        validatioSetup()
+        validationSetup()
     }
     private fun spinnerSetup(){
         countryspinner = findViewById(R.id.spinnerSelectCountry)
@@ -42,7 +50,8 @@ class EditProfileActivity : AppCompatActivity() {
         }
     }
 
-    private fun validatioSetup() {
+
+    private fun validationSetup() {
         btn = findViewById(R.id.buttonClose)
         firstName = findViewById(R.id.etFirstName)
         lastName = findViewById(R.id.etLastName)
@@ -51,34 +60,57 @@ class EditProfileActivity : AppCompatActivity() {
         perState = findViewById(R.id.etState)
         perZipCode = findViewById(R.id.etZipCode)
         perMobileNo = findViewById(R.id.etMobileNumber)
+        fnError = findViewById(R.id.fnError)
+        lnError = findViewById(R.id.lnError)
+        adrError = findViewById(R.id.adrError)
+        stateError = findViewById(R.id.stateError)
+        cityError = findViewById(R.id.cityError)
+        zipError = findViewById(R.id.zipError)
+        mobileError = findViewById(R.id.mobileError)
         btn.setOnClickListener {
             if(firstName.text.isEmpty() && firstName.text.count() <= 5){
-                firstName.error = "Enter first Name"
-            } else if (firstName.text.count() < 5) {
-                firstName.error = "Name must contain at least 5 characters"
+                fnError.visibility = View.VISIBLE
+               // firstName.setError("The erroe is somtirhoags", getDrawable(R.drawable.baseline_arrow_drop_down_24))
+                //firstName.error = "Enter your first name"
             }
+//            else if (firstName.text.count() < 5) {
+//                firstName.error = "Name must contain at least 5 characters"
+                //firstName.setError("",getDrawable(R.drawable.custom_shape))
+           // }
             if(lastName.text.isEmpty()){
-                lastName.error = "Enter your last name"
+              //  lastName.error = "Enter your last name"
+                lnError.visibility = View.VISIBLE
             }
             if(perAddress.text.isEmpty()){
-                perAddress.error = "Enter your email address"
+               // perAddress.error = "Enter your email address"
+                adrError.visibility = View.VISIBLE
             }
             if(perCity.text.isEmpty()){
-                perCity.error = "Enter your city"
+               // perCity.error = "Enter your city"
+                cityError.visibility = View.VISIBLE
             }
             if(perState.text.isEmpty()){
-                perState.error = "Enter your state"
+               // perState.error = "Enter your state"
+                stateError.visibility = View.VISIBLE
             }
             if(perMobileNo.text.isEmpty()){
-                perMobileNo.error = "Enter your mobile no"
+                //perMobileNo.error = "Enter your mobile no"
+                mobileError.visibility = View.VISIBLE
             }
             if(perZipCode.text.isEmpty()){
-                perZipCode.error = "Enter your Zip code"
-            } else if (perZipCode.text.count() <=5) {
-                perZipCode.error = "Enter Valid Zip Code"
-            } else if (perZipCode.text.count() > 6) {
-                perZipCode.error = "Enter Valid Zip Code"
+                //perZipCode.error = "Enter your Zip code"
+                zipError.visibility = View.VISIBLE
             }
+//            else if (perZipCode.text.count() <=5) {
+//                perZipCode.error = "Enter Valid Zip Code"
+//            } else if (perZipCode.text.count() > 6) {
+//                perZipCode.error = "Enter Valid Zip Code"
+//            }
+//            else if (perZipCode.text.count() <=5) {
+//                perZipCode.error = "Enter Valid Zip Code"
+//            } else if (perZipCode.text.count() > 6) {
+//                perZipCode.error = "Enter Valid Zip Code"
+//            }
             else {
                 Toast.makeText(this, "You data had been saved successfully",Toast.LENGTH_LONG).show()
             }
